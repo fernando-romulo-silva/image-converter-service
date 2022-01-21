cda720c3c7375fd478854c1103436f0c442327a0
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2022-01-21 16:46:23 -0300

    news

--- src/main/java/org/imageconverter/util/controllers/imageconverter/ImageConverterRequestInterface.java
+++ src/main/java/org/imageconverter/util/controllers/imageconverter/ImageConverterRequestInterface.java
@@ -3,7 +3,7 @@ package org.imageconverter.util.controllers.imageconverter;
 import org.imageconverter.domain.convertion.ExecutionType;
 import org.springframework.web.multipart.MultipartFile;
 
-public interface ImageConverterRequestInterface {
+public sealed interface ImageConverterRequestInterface permits ImageConverterRequest, ImageConverterRequestArea {
 
     MultipartFile file();
     
