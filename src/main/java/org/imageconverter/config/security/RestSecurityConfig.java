560b88b88e5cb10abca45911e1130542e220a24a
Fernando Romulo da Silva <fernando.romulo.silva@gmail.com>
2022-07-15 08:41:49 +0100

    news

--- src/main/java/org/imageconverter/config/security/RestSecurityConfig.java
+++ src/main/java/org/imageconverter/config/security/RestSecurityConfig.java
@@ -6,10 +6,11 @@ import static org.springframework.http.HttpMethod.POST;
 
 import org.imageconverter.config.filter.CsrfLoggerFilter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.csrf.CsrfTokenRepository;
@@ -23,7 +24,7 @@ import org.springframework.security.web.firewall.HttpFirewall;
  * @author Fernando Romulo da Silva
  */
 @Configuration
-public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
+public class RestSecurityConfig {
 
     private final String[] swaggerUiURL = { //
 	    "/v3/api-docs/**", //
@@ -44,16 +45,62 @@ public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
 		    final HttpFirewall allowUrlEncodedSlashHttpFirewall, //
 		    final CsrfTokenRepository httpSessionCsrfTokenRepository) {
 	//
-	super(true); // disable default configuration
 	this.authenticationSuccessHandler = authenticationSuccessHandler;
 	this.allowUrlEncodedSlashHttpFirewall = allowUrlEncodedSlashHttpFirewall;
 	this.csrfTokenRepository = httpSessionCsrfTokenRepository;
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
+    
+    @Bean
+    public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
+     
+	final var restUrl = "/rest/**";
+	
+	http.addFilterAfter(new CsrfLoggerFilter(), CsrfFilter.class) //
+			.securityContext() //
+			.and().exceptionHandling() //
+			.and().servletApi() //
+			.and().httpBasic() //
+			//
+			.and().authorizeRequests() //
+			//
+			/*--*/.antMatchers(swaggerUiURL) //
+			/*------*/.permitAll()
+			//
+			/*--*/.antMatchers(GET, restUrl) // /rest/images/type
+			/*------*/.hasAnyRole("USER") // , "GUEST"
+			//
+			/*--*/.antMatchers(POST, restUrl) //
+			/*------*/.hasRole("USER") //
+			//
+//			/*--*/.antMatchers("/actuator/**")
+//			/*------*/.hasRole("ADMIN") 			
+			//
+			/*--*/.antMatchers(DELETE, restUrl) //
+			/*------*/.access("hasRole('ROLE_ADMIN') or hasIpAddress('127.0.0.1') or hasIpAddress('0:0:0:0:0:0:0:1')") //
+			//
+			/*--*/.antMatchers(restUrl) //
+			/*------*/.hasAnyRole("ADMIN", "USER")
+			//
+			.and().formLogin() // disable redirect
+			/*------*/.successHandler(authenticationSuccessHandler) //
+			/*------*/.failureHandler(new SimpleUrlAuthenticationFailureHandler()) //
+			//
+			.and().logout() //
+			/*------*/.logoutSuccessUrl("/") //
+			/*------*/.invalidateHttpSession(true)//
+			/*------*/.clearAuthentication(true)//
+			//
+			.and().csrf() //
+//			/*------*/.disable() //
+			/*------*/.csrfTokenRepository(csrfTokenRepository)//
+			/*------*/.ignoringAntMatchers("/actuator/**");
+ 
+        http.headers().frameOptions().sameOrigin();
+ 
+        return http.build();
+    }
+    
     protected void configure(final HttpSecurity http) throws Exception {
 
 	final var restUrl = "/rest/**";
@@ -99,13 +146,8 @@ public class RestSecurityConfig extends WebSecurityConfigurerAdapter {
 			/*------*/.ignoringAntMatchers("/actuator/**");
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void configure(final WebSecurity web) throws Exception {
-	super.configure(web);
-	web.ignoring().antMatchers(swaggerUiURL);
-	web.httpFirewall(allowUrlEncodedSlashHttpFirewall);
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> { web.ignoring().antMatchers(swaggerUiURL); web.httpFirewall(allowUrlEncodedSlashHttpFirewall);};
     }
 }
